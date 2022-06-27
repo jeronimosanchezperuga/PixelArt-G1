ec004303c0a79e01a185780d5ced7989e62d2ce4
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-06-27 16:07:51 -0300

    clase 27/6

---
+++ Assets/Scenes/Haunted House/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.x - target.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x + offset,transform.position.y,transform.position.z);
+    }
+}
