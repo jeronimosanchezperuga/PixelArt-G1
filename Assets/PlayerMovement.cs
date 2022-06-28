ab075f3e7ad3ca56502f1f1c24c3c33801d291e5
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-06-27 22:44:43 -0300

    New character controller Brackeys implemented

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController2D characterController;
+    public float runSpeed = 40f;
+    float horizontalMove;
+    bool jump = false;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        characterController.Move(horizontalMove * Time.deltaTime,false,jump);
+        jump = false;
+    }
+}
