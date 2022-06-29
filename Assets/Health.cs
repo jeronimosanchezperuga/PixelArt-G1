df45547551236949e79ee5170387e108a13d411f
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-06-29 08:35:21 -0300

    Patrol between two targets

---
+++ Assets/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int lives = 3;
+    public int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            lives--;
+            if (lives <= 0)
+            {
+                //die
+            }
+        }
+        //update UI
+    }
+}
