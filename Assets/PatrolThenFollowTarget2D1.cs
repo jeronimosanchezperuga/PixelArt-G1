c6f89a39674a6bd7e5e23b77685e65dc629c99de
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-07-11 14:32:30 -0300

    Incomplete follow script USS

---
+++ Assets/PatrolThenFollowTarget2D1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolThenFollowTarget2D1 : MonoBehaviour
+{
+    public Transform patrollerTR;
+    [SerializeField]
+    Transform target1;
+    [SerializeField]
+    Transform target2;
+    [SerializeField] Transform targetPplayer;
+    [SerializeField]
+    float speed;
+    Transform currentTarget;
+    public bool toLeft = true;
+    [SerializeField]
+    private float distanceThreshold = 1f;
+    public float direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (!target1)
+        {
+            target1 = targetPplayer;
+        }
+        currentTarget = target1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction= (patrollerTR.position.x - currentTarget.position.x);
+        if (direction < 0)
+        {
+            patrollerTR.eulerAngles = Vector3.up * 180;
+        }
+        else
+        {
+            patrollerTR.eulerAngles = Vector3.down * 0;
+        }
+
+        var step = speed * Time.deltaTime;
+        patrollerTR.position = Vector3.MoveTowards(patrollerTR.position, currentTarget.position, step);
+
+        if (Vector2.Distance(patrollerTR.position,targetPplayer.position) < distanceThreshold)
+        {
+            currentTarget = targetPplayer;
+        }
+    }
+}
