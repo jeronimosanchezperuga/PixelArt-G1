fcb6cec147af293e98911b0febc81ed695ddd19d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-09-08 12:20:49 -0300

    Controls, jump, ground check and platforms

---
+++ Assets/00CLASE04/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float fuerzaAvance;
+    public float fuerzaSalto;
+    public Rigidbody2D rb;
+    public Transform tr;
+    public bool estaEnElPiso;
+    public LayerMask layer;
+    public float fuerzaAvanceEnElAire;
+    public float fuerzaAvanceEnElPiso;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movX*fuerzaAvance, rb.velocity.y);
+
+        Vector3 escalaActual = tr.localScale;
+        if (movX > 0)
+        {
+            escalaActual.x = 1;
+            tr.localScale = escalaActual;
+        }
+        if (movX <0)
+        {
+            escalaActual.x = -1;
+            tr.localScale = escalaActual;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && estaEnElPiso)
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto,ForceMode2D.Impulse);
+        }
+
+        estaEnElPiso = Physics2D.OverlapCircle(tr.position, 0.15f, layer);
+        //if (!estaEnElPiso)
+        //{
+        //    fuerzaAvance = fuerzaAvanceEnElAire;
+        //}
+        //else
+        //{
+        //    fuerzaAvance = fuerzaAvanceEnElPiso;
+        //}
+
+    }
+
+}
---
+++ Assets/Fade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    Image m_Image; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Image.CrossFadeAlpha(0, 2f, true);
+    }
+}
